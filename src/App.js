b1d9e9c1b312ecbe2940969a9c350bebe4157fad
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-04-09 18:22:10 +0530

    created the basic notes application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import NotesList from "./components/NotesList";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [serachText, setSearchText] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
+    if (savedNotes) setNotes(savedNotes);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+  }, [notes]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      text: text,
+      date: date.toLocaleDateString(),
+      id: nanoid(),
+    };
+    setNotes([...notes, newNote]);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id != id);
+    setNotes(newNotes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${darkMode && "dark-mode"}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(serachText)
+          )}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = ({handleToggleDarkMode}) => {
+  return (
+    <div className="header">
+      <h1>Notes</h1>
+      <button onClick={()=>handleToggleDarkMode((prev)=>!prev)} className="save">Toggle Theme</button>
+    </div>
+  );
+};
+
+export default Header;
